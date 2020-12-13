a6c755dd24a6f99a687ff0803d6ccd34e8157d56
shubham <tech.shubham574@gmail.com>
2020-12-13 09:17:33 +0530

    modify inheritance class

--- src/com/oops/example/Inheritance_IsA_Rel.java
+++ src/com/oops/example/Inheritance_IsA_Rel.java
@@ -1,7 +1,5 @@
 package com.oops.example;
 
-import java.util.PrimitiveIterator.OfDouble;
-
 //INHERITANCE EXAMPLE
 /*
 
@@ -18,17 +16,65 @@ class B extends A
     {
     	System.out.println("this is go method of class b");
     }
-	
+}
+
+*/
+
+//___________________________________________________________________________________________
+
+/*
+    Types of inheritance------
+    
+    1) Single inheritance
+    2) multilevel inheritance
+    3) mutiple inheritance 
+    4) hierarchical inheritance
+    5) hybtid inheritance  
+ */
+
+// Single inheritance ------- when a class inherits another class is called single inheritance
+
+//SINGLE INHERITANCE + METHOD OVERRIDING
+
+class Animal
+{
+	public Animal() {
+		System.out.println("Animal class called");
+	}
+	void eat()//common method
+	{
+		System.out.println("Animal eat many foods");
+	}
+}
+class Dog extends Animal
+{
+	public Dog() {
+	     System.out.println("dog class called");	
+	}
+	void eat(String s)//parameterized method
+	{
+		System.out.println("Dog likes "+s);
+	}
+}
+class Cat extends Dog
+{
+    public Cat() {
+		System.out.println("cat class called");
+	}
+    void eat()//common method
+    {
+    	System.out.println("cat drink milk");
+    }
 }
 
 
-*/
+
 
 //_______________________________________________________________________________________
 
 
 
-public class Inheritance_IsA_Rel //extends B
+public class Inheritance_IsA_Rel extends Cat //extends B
 {	
 	public static void main(String[] args) {
         
@@ -37,7 +83,9 @@ public class Inheritance_IsA_Rel //extends B
 //		Inheritance_IsA_Rel rels=new Inheritance_IsA_Rel();
 //      rels.go();
 		
-		
+		Inheritance_IsA_Rel rels = new Inheritance_IsA_Rel();
+		rels.eat();
+		rels.eat("mangoes");
 		
 	}
 
