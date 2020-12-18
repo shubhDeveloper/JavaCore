11abb4c30e819416a49f8f2ecc9470eb942aba4a
shubham <tech.shubham574@gmail.com>
2020-12-18 12:07:35 +0530

    added Abstract and interface

--- src/com/oops/example/FinalKeyword.java
+++ src/com/oops/example/FinalKeyword.java
@@ -27,6 +27,8 @@ class Vehical
 
 //2) : Final method - If you make any method as a final ,you can't override it.
 
+/*
+
 class Bike
 {
 	final void start()
@@ -34,25 +36,39 @@ class Bike
 		System.out.println("Bike start");
 	}
 }
-class Splendor 
+class Splendor extends Bike
 {
-	void start()
+	void start()  ///compile time error bcz. we can't override it 
 	{
 		System.out.println("spledor start");
 	}
 }
 
+*/
+
+//3) Final class - you make any class as final we can't extends
+/*
+final class Bike
+{
+	
+}
+class Splendor extends Bike  // we cant extend bcz. parent class is final
+{
+	
+}
+*/
+
 public class FinalKeyword {
 
 	public static void main(String[] args) {
 
-//	  Final variable.......
+//1)  Final variable.......
 		
 //	  Vehical v = new Vehical();	
 //	  v.speed();
 //	  v.display();
 		
-//	  Final method
+
 	  
 
 	}
