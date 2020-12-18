f17e9fffc6f6672ace08f8debfe33355bf2a52ec
shubham <tech.shubham574@gmail.com>
2020-12-18 13:28:12 +0530

    master branch

--- src/com/oops/example/StringInJava.java
+++ src/com/oops/example/StringInJava.java
@@ -3,7 +3,7 @@ package com.oops.example;
 public class StringInJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello this is master branch");
 
 	}
 
