e13870985ca08db930634ce9d03f12dbaccfb072
shubham <tech.shubham574@gmail.com>
2021-01-05 21:46:36 +0530

    added exception

---
+++ src/com/java/string/Strings.java
@@ -0,0 +1,17 @@
+package com.java.string;
+
+//String = 1) String is non primitive data type 
+//         2) String is a sequence of character or an array of character
+//         3) we can create string class object by : String s = new String(); It will immutable object
+//         4) to create string there are three classes :-  1) String   2) StringBuffer  3)StringBuilder
+//         5) String constant pool was present in method area until 1.6 version but after 1.6 ver it was shiped in heap area-
+//         6) String constant pool is special memory location present in heap area which is used to store string literals
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
